706e6f72f9ea11d4aa4115ba1726123bab189f4f
MrSullivan <>
2021-05-17 21:46:58 +0000

    if and else comman with return

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    answer =  smallest(2, 7)
+    print("Smallest: " + str(answer))
+
+def smallest(number1, number2):
+  if(number1 > number2):
+    return number2
+  else:
+    return number1
 
 if __name__ == '__main__':
     main()
